56a7842f4e64300aa352391212f90f5505cbf466
Sia <Sia.Simeonova@outlook.com>
2016-03-31 09:29:38 +0300

    new classes

---
+++ SoundCloud/src/DAO/IUserDAO.java
@@ -0,0 +1,14 @@
+package DAO;
+
+import POJO.User;
+
+public interface IUserDAO {
+	public int addUser(User user) throws UserDAOException;
+	public boolean  isThereSuchUser(String user) throws UserDAOException;
+	public int editUser(User user) throws UserDAOException;
+	public void deteteUser(User user) throws UserDAOException;
+	public User getUser(String name) throws UserDAOException;
+	public int followUser(User user,User userToFollow) throws UserDAOException;
+	public int getFollowing(User user) throws UserDAOException;
+	public int getFollowers(User user) throws UserDAOException;
+}
---
+++ SoundCloud/src/servlets/login.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.UserDAO;
+import DAO.UserDAOException;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String user=request.getParameter("username");
+		String pass=request.getParameter("password");
+		boolean isThereSuchUser=false;
+			try {
+				isThereSuchUser=new UserDAO().isThereSuchUser(user);
+			} catch (UserDAOException e) {
+				e.printStackTrace();
+			}
+		if(isThereSuchUser){
+			HttpSession session=request.getSession();
+			session.setAttribute("user", user);
+			response.sendRedirect("./header.jsp");
+		}
+		else{
+			response.getWriter().println("Nema takuv");
+		}
+	}
+
+}
